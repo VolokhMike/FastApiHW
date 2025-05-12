2b101572a88d79290be202a102cb869efe331542
La vita <mikimv09@gmail.com>
2025-05-12 16:51:45 +0200

    add new hw

---
+++ Token.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI, HTTPException, Header, Path, Query
+from typing import Optional
+from datetime import datetime
+
+
+app = FastAPI()
+
+
+@app.get("/user/{user_id}")
+async def get_user(user_id: int = Path(..., description="user id"), timestamp: Optional[str] = Query(None, description="Ask Time"), x_client_version: str = Header(...,  description="Version")):
+
+    if not timestamp:
+        timestamp = datetime.now()
+
+    if timestamp is not None:
+        raise HTTPException(400, "Timestamp is not definded ")
+
+    return {
+        "user_id": user_id,
+        "timestamp": timestamp,
+        "client_version": x_client_version,
+        "message": f"Hello user {user_id}"
+    }
+
